03e67065deac03711bfb7f5ce56613c43b0ecd6f
Xiangyu Sun <xiangyu.sun@dnata.com>
2019-06-20 18:54:19 +0400

    Initial Commit

---
+++ SPMDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SPMDemo
+//
+//  Created by 孙翔宇 on 20/06/2019.
+//  Copyright © 2019 孙翔宇. All rights reserved.
+//
+
+import SwiftUI
+import BowlingKit
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
